834d28e093f0140814081daeef564371cba3353d
Andrey Polyskalov <contact@polyskalov.com>
2017-02-23 00:41:32 +0500

    Знаки на кнопках  [+] и [-] заменены на HTML-эквиваленты
    IE, например, не знает &plus;, а &#43; все знают.

--- views/index.php
+++ views/index.php
@@ -1,4 +1,4 @@
-<a class="map-zoomin" href="#">&plus;</a>
-<a class="map-zoomout" href="#">&minus;</a>
+<a class="map-zoomin" href="#">&#43;</a>
+<a class="map-zoomout" href="#">&#8722;</a>
 <div class="map"></div>
 <div class="kgb">путеводитель по интересным местам города Качканар</div>
