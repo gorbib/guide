e7f054fcb596f30c9819ccf4981232385ea9497e
Andrey Polyskalov <a@polyskalov.com>
2017-03-08 20:57:16 +0500

    Все фотографии одинакового размера
    Соотношение сторон теперь 16:9, а ещё они подстраиваются под высоту и
    ширину (cover).
    
    А то они были слишком высокие, отличались по высоте на разных
    страницах, в зависимости от первой фотографии, а по бокам у многих были
    белые полосы.

--- views/place.php
+++ views/place.php
@@ -2,7 +2,7 @@
 <?php if ($admin) : ?>
 	<p style="text-align:center"><a href="/+/<?=$place['id']?>">изменить</a></p>
 <?php endif; ?>
-<div class="fotorama" data-allowfullscreen="native" data-width="100%" class="place__photos">
+<div class="fotorama" data-allowfullscreen="native" data-ratio="16/9" data-width="100%" data-fit="cover" class="place__photos">
 	<?php foreach ($place['images'] as $image) : ?>
 		<img src="<?=$image['url']?>" data-caption="<?=$image['caption']?>">
 	<?php endforeach;?>
