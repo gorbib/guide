2977bc209119fba1c81db835fbfa29f320a4e131
Andrey Polyskalov <a@polyskalov.com>
2017-03-13 00:54:57 +0500

    Добавлен атрибут alt к фотографиям
    По спецификации alt должен быть у каждой картинки — это раз. А во
    вторых — это поисковая оптимизация.

--- views/place.php
+++ views/place.php
@@ -2,9 +2,9 @@
 <?php if ($admin) : ?>
 	<p style="text-align:center"><a href="/+/<?=$place['id']?>">изменить</a></p>
 <?php endif; ?>
-<div class="fotorama" data-allowfullscreen="native" data-ratio="16/9" data-width="100%" data-fit="cover" class="place__photos">
+<div class="fotorama" data-allowfullscreen="native" data-ratio="16/9" data-width="100%" data-fit="cover">
 	<?php foreach ($place['images'] as $image) : ?>
-		<img src="<?=$image['url']?>" data-caption="<?=$image['caption']?>">
+		<img src="<?=$image['url']?>" data-caption="<?=$image['caption']?>" alt="<?=$place['title']?> в Качканаре. <?=$image['caption']?>">
 	<?php endforeach;?>
 </div>
 
