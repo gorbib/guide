b3f706863a0cb6555121fda0bfed10d4f98f807d
Andrey Polyskalov <a@polyskalov.com>
2017-03-14 02:22:20 +0500

    Добавлены всякие картинки для интеграции с системой
    Добавлен манифест веб приложений, манифест для 10 винды и разношёрстные
    картинки к ним.

--- views/layout.php
+++ views/layout.php
@@ -16,7 +16,12 @@
     <meta property="og:site_name" content="Путеводитель по Качканару">
     <meta property="og:description" content="<?=$description?>">
 
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#3c3c3c">
+    <meta name="theme-color" content="#ffffff">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
