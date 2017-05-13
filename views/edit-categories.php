b22b357f13a49684b69b5a711bb29116441cdeac
Andrey Polyskalov <a@polyskalov.com>
2017-05-13 14:33:28 +0500

    Слегка изменена форма добавления категории

--- views/edit-categories.php
+++ views/edit-categories.php
@@ -1,5 +1,5 @@
 <h1 class="page-title">Категории</h1>
-<form class="category-list container card">
+<form class="category-list container">
     <?php foreach ($categoriesList as $category) : ?>
     <div class="category-list__item" data-category-id="<?=$category['id']?>">
         <span class="category-list__item__name"><?=$category['name']?></span>
@@ -7,10 +7,12 @@
     </div>
     <?php endforeach; ?>
 </form>
-<form class="container category-list__new-form" action="/+/categories" method="post">
-    <input class="category-list__new-form__input" type="text" name="name" placeholder="Название" required>
-    <input class="category-list__new-form__input" type="text" name="alias" pattern="[a-z0-9-]+" placeholder="Адрес" required>
-    <button class="btn">Добавить</button>
+<form class="container category-edit-form" action="/+/categories" method="post">
+    <div class="category-edit-form__main">
+        <input class="category-edit-form__field" type="text" name="name" placeholder="Название" maxlength="60" required>
+        <input class="category-edit-form__field" type="text" name="alias" pattern="[a-z0-9-]+" placeholder="Адрес" required>
+    </div>
+    <button class="btn category-edit-form__submit-button">Добавить</button>
 </div>
 
 <?php ob_start() ?>
