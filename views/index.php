da623e0e3fee4705d5fd2d464c44ac3ce8461d74
Andrey Polyskalov <a@polyskalov.com>
2019-12-30 03:36:24 +0500

    Start redesigning

--- views/index.php
+++ views/index.php
@@ -1,4 +1,6 @@
-<a class="map-zoomin" href="#" title="Приблизить карту">&#43;</a>
-<a class="map-zoomout" href="#" title="Отдалить карту">&#8722;</a>
-<div class="map"></div>
-<div class="kgb">путеводитель по интересным местам города Качканар</div>
+<div class="index-bg">
+    <div class="index-bg__text">
+        <h1>Путеводитель по Качканару</h1>
+        <h2>Подборка интересных мест и достопримечательностей города</h2>
+    </div>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -25,7 +25,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Serif+Caption&amp;subset=cyrillic">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,500&display=swap&subset=cyrillic" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ilyabirman-likely/2.2.1/likely.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.min.css">
     <?php if ($admin) : ?>
@@ -36,59 +36,43 @@
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <aside class="sidebar">
-        <img src="/images/mountain.svg" class="sidebar__mountain" alt="">
-        <div class="sidebar__body">
-
-            <header class="header">
-                <label for="navigation-toggler" class="navigation-toggle"></label>
-                <a class="logo" href="/">Качканар</a>
-            </header>
-
-            <input type="checkbox" id="navigation-toggler" hidden>
-
-            <ul class="navigation">
-            <?php foreach ($categories as $category) : ?>
-            <li><a href="/<?=$category['alias']?>"><?=$category['name']?><sup><?=$category['count']?></sup></a></li>
+    <header class="header">
+        <a href="/" class="header__logo">В Качканаре</a>
+        <ul class="header-menu">
+            <?php foreach (/** @var array $categories */ $categories as $cat) : ?>
+                <li><a href="/<?=$cat['alias']?>" class="header-menu__item <?=($cat['id'] === $category['id'] ? 'header-menu__item_active':'')?>"><?=$cat['name']?></a></li>
             <?php endforeach;?>
-
-            <?php if ($admin) : ?>
-            <li><a href="/+/categories" class="navigation__button">изменить категории</a></li>
-            <li><a href="/+/" class="navigation__button">добавить место</a></li>
-            <?php endif; ?>
-            </ul>
-        </div>
-        <ul class="sidebar__social">
-            <li><a class="fb" target="_blank" href="https://www.facebook.com/gorbib"></a></li>
-            <li><a class="vk" target="_blank" href="https://vk.com/gorbib"></a></li>
-            <li><a class="ig" target="_blank" href="https://www.instagram.com/gorbib/"></a></li>
-            <li><a class="yt" target="_blank" href="https://www.youtube.com/user/kchlib"></a></li>
         </ul>
-    </aside>
-
+        <?php if ($admin) : ?>
+        <div class="header-menu header-menu_admin">
+            <a href="/+/categories" class="header-menu__item btn">изменить категории</a>
+            <a href="/+/" class="header-menu__item btn">добавить место</a>
+        </div>
+        <?php endif; ?>
+    </header>
     <main class="main">
-        <article class="content">
-            <?php echo $content; ?>
-        </article>
+            <article class="content">
+                <?php echo $content; ?>
+            </article>
 
-        <footer class="footer">
-            <div class="footer__something">
-                <div id="vk_groups" style="height: 400px;"></div>
-            </div>
-            <div class="footer__main">
-                <p class="copyright">Информация собрана сотрудниками <a href="http://gorbib.org.ru" target="_blank">качканарской библиотеки им. Селянина</a> к&nbsp;60-летию города.
-                <a href="mailto:gorbib@yandex.ru?subject=Путеводитель">Напишите нам</a>, если есть вопросы или предложения.</p>
-                <div id="openstat4031444" class="footer__stat-block" title="Сколько человек было у нас на сайте в прошлом месяце; Сколько было вчера; И сколько прямо сейчас"></div>
-                <div>Расскажите о путеводителе</div>
-                <div class="likely likely-light" data-via="gorbib" data-url="https://вкачканаре.рф" data-title="Путеводитель по Качканару">
-                    <div class="vkontakte">ВКонтакте</div>
-                    <div class="facebook">фейсбук</div>
-                    <div class="twitter">твиттер</div>
-                    <div class="odnoklassniki">одноклассники</div>
+            <footer class="footer">
+                <div class="footer__something">
+                    <div id="vk_groups" style="height: 400px;"></div>
                 </div>
-            </div>
-        </footer>
-    </main>
+                <div class="footer__main">
+                    <p class="copyright">Информация собрана сотрудниками <a href="http://gorbib.org.ru" target="_blank">качканарской библиотеки им. Селянина</a> к&nbsp;60-летию города.
+                    <a href="mailto:gorbib@yandex.ru?subject=Путеводитель">Напишите нам</a>, если есть вопросы или предложения.</p>
+                    <div id="openstat4031444" class="footer__stat-block" title="Сколько человек было у нас на сайте в прошлом месяце; Сколько было вчера; И сколько прямо сейчас"></div>
+                    <div>Расскажите о путеводителе</div>
+                    <div class="likely likely-light" data-via="gorbib" data-url="https://вкачканаре.рф" data-title="Путеводитель по Качканару">
+                        <div class="vkontakte">ВКонтакте</div>
+                        <div class="facebook">фейсбук</div>
+                        <div class="twitter">твиттер</div>
+                        <div class="odnoklassniki">одноклассники</div>
+                    </div>
+                </div>
+            </footer>
+        </main>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
     <script src="/js/main.js"></script>
@@ -106,28 +90,5 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ilyabirman-likely/2.2.1/likely.js"></script>
 
-
-    <!-- For old browsers -->
-    <script src="https://yastatic.net/browser-updater/v1/script.js"></script>
-    <script>
-    var yaBrowserUpdater = new ya.browserUpdater.init({
-        "lang":"ru",
-        "browsers":{
-            "yabrowser":"15.12",
-            "chrome":"54",
-            "ie":"10",
-            "opera":"41",
-            "safari":"8",
-            "fx":"49",
-            "iron":"35",
-            "flock":"Infinity",
-            "palemoon":"25",
-            "camino":"Infinity",
-            "maxthon":"4.5",
-            "seamonkey":"2.3"
-        },
-        "theme":"yellow"
-    });
-    </script>
 </body>
 </html>
